f865ccd55837e0c4e5b6f9217ad845ccfc4e1569
Auvigoo <auvigooa@gmail.com>
2022-09-22 01:38:26 -0400

    add getters and setters for Territory class

--- Map.cpp
+++ Map.cpp
@@ -1,5 +1,87 @@
+#pragma once
 #include "Map.h"
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+Territory::Territory() {
+    owner = nullptr;
+    numOfArmies = 0;
+    name = "";
+    x = 0;
+    y = 0;
+}
 
-int testing2(){
-    return 456;
+Territory::Territory(Territory &t) {
+    owner = t.owner;
+    numOfArmies = t.numOfArmies;
+    name = t.name;
+    adjacentTerritories = t.adjacentTerritories;
+    x = t.x;
+    y = t.y;
+}
+
+Territory::Territory(Player *owner, Continent* continent, int numOfArmies, std::string name, vector<Territory *>, int x, int y) {
+    this->owner = owner;
+    this->continent = continent;
+    this->numOfArmies = numOfArmies;
+    this->name = name;
+    this->adjacentTerritories = adjacentTerritories;
+    this->x = x;
+    this->y = y;
+}
+
+Player* Territory::getOwner() {
+    return this->owner;
+}
+
+int Territory::getNumOfArmies() {
+    return this->numOfArmies;
+}
+
+string Territory::getName() {
+    return this->name;
+}
+
+vector<Territory*> Territory::getAdjacentTerritories() {
+    return this->adjacentTerritories;
+}
+
+int Territory::getX(){
+    return this->x;
+}
+
+int Territory::getY(){
+    return this->y;
+}
+
+void Territory::setOwner(Player* player){
+    this->owner = player;
+}
+
+void Territory::setContinent(Continent* continent){
+    this->continent = continent;
+}
+void Territory::setNumOfArmies(int numOfArmies){
+    this->numOfArmies = numOfArmies;
+}
+
+void Territory::setName(string name){
+    this->name = name;
+}
+
+void Territory::setAdjacentTerritories(vector<Territory*> territories){
+    this->adjacentTerritories = territories;
+}
+
+void Territory::setX(int x){
+    this->x = x;
+}
+
+void Territory::setY(int y){
+    this->y = y;
+}
+
+void Territory::addAdjacentTerritory(Territory *territory) {
+    adjacentTerritories.push_back(territory);
 }
--- Map.h
+++ Map.h
@@ -1,1 +1,50 @@
-int testing2();
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class Map{
+
+};
+
+class Continent{
+
+};
+
+class Player;
+
+class Territory{
+    private:
+        Player* owner;
+        Continent* continent;
+        int numOfArmies;
+        string name;
+        vector<Territory*> adjacentTerritories;
+        int x, y;
+
+    public:
+        Territory();
+        Territory(Territory &t);
+        Territory(Player* owner, Continent* continent, int numOfArmies, string name, vector<Territory*>, int x, int y);
+
+        Player* getOwner();
+        Continent* getContinent();
+        int getNumOfArmies();
+        string getName();
+        vector<Territory*> getAdjacentTerritories();
+        int getX();
+        int getY();
+
+        void setOwner(Player* player);
+        void setContinent(Continent* continent);
+        void setNumOfArmies(int numOfArmies);
+        void setName(string name);
+        void setAdjacentTerritories(vector<Territory*> territories);
+        void setX(int x);
+        void setY(int y);
+
+        void addAdjacentTerritory(Territory* territory);
+
+
+
+};
