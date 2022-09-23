42a2d96b2e25a4a45b21cd23f6ae1a8f5bd504c0
Auvigoo <auvigooa@gmail.com>
2022-09-22 20:04:15 -0400

    add DFS algorithm for map and function to check if map is connected

--- Map.cpp
+++ Map.cpp
@@ -1,9 +1,13 @@
 #pragma once
 #include "Map.h"
+#include "Player.h"
 #include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 using std::string;
 using std::vector;
+using std::cout;
 Territory::Territory() {
     owner = nullptr;
     numOfArmies = 0;
@@ -12,13 +16,20 @@ Territory::Territory() {
     y = 0;
 }
 
-Territory::Territory(Territory &t) {
+// Copy constructor
+Territory::Territory(const Territory &t) {
     owner = t.owner;
     numOfArmies = t.numOfArmies;
     name = t.name;
     adjacentTerritories = t.adjacentTerritories;
     x = t.x;
     y = t.y;
+}
+
+Territory::Territory(string name, int x, int y) {
+    this->name = name;
+    this->x = x;
+    this->y = y;
 }
 
 Territory::Territory(Player *owner, Continent* continent, int numOfArmies, std::string name, vector<Territory *>, int x, int y) {
@@ -31,28 +42,13 @@ Territory::Territory(Player *owner, Continent* continent, int numOfArmies, std::
     this->y = y;
 }
 
-Player* Territory::getOwner() {
-    return this->owner;
-}
-
-int Territory::getNumOfArmies() {
-    return this->numOfArmies;
-}
-
-string Territory::getName() {
-    return this->name;
-}
-
-vector<Territory*> Territory::getAdjacentTerritories() {
-    return this->adjacentTerritories;
-}
-
-int Territory::getX(){
-    return this->x;
-}
-
-int Territory::getY(){
-    return this->y;
+Territory::Territory(Player *owner, Continent* continent, int numOfArmies, std::string name, int x, int y) {
+    this->owner = owner;
+    this->continent = continent;
+    this->numOfArmies = numOfArmies;
+    this->name = name;
+    this->x = x;
+    this->y = y;
 }
 
 void Territory::setOwner(Player* player){
@@ -84,4 +80,105 @@ void Territory::setY(int y){
 
 void Territory::addAdjacentTerritory(Territory *territory) {
     adjacentTerritories.push_back(territory);
+}
+
+Map::Map(){}
+
+Map::Map(const Map &map) {
+    this ->allTerritories = map.allTerritories;
+    this->allContinents = map.allContinents;
+}
+
+void Map::setAllTerritories(vector<Territory *> territories) {
+    this->allTerritories = territories;
+}
+
+void Map::setAllContinents(vector<Continent *> continents) {
+    this->allContinents = continents;
+}
+
+void Map::addTerritory(Territory *t) {
+    allTerritories.push_back(t);
+}
+
+void Map::addContinent(Continent *c) {
+    allContinents.push_back(c);
+}
+
+void Map::dfs(vector<std::string> *visitedTerritories, Territory *currentTerritory) {
+
+    vector<Territory*> currentAdjacentTerritories = currentTerritory->getAdjacentTerritories();
+
+    //if territory has already been visited
+    if(std::find(visitedTerritories->begin(), visitedTerritories->end(), currentTerritory->getName()) != visitedTerritories->end()){
+        return;
+    }
+
+    cout << "visiting territory: " << currentTerritory->getName() << std::endl;
+    // add current territory to visited territories
+    visitedTerritories->push_back(currentTerritory->getName());
+    //visit adjacent territories
+    for(int i = 0; i < currentAdjacentTerritories.size(); i++){
+        dfs(visitedTerritories, currentAdjacentTerritories.at(i));
+    }
+}
+
+bool Map::isConnectedTerritories() {
+
+    vector<std::string> visitedTerritories;
+    Territory* startTerritory = allTerritories.at(0);
+
+    // start DFS at first territory
+    dfs(&visitedTerritories, startTerritory);
+
+    // check if all territories have been visited after DFS
+    for (int i = 0; i < allTerritories.size(); i++){
+        if(std::find(visitedTerritories.begin(), visitedTerritories.end(), allTerritories.at(i)->getName()) == visitedTerritories.end()){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main(){
+    Territory* t1 = new Territory("territory1", 2, 3);
+    Territory* t2 = new Territory("territory2", 2, 3);
+    Territory* t3 = new Territory("territory3", 2, 3);
+    Territory* t4 = new Territory("territory4", 2, 3);
+    Territory* t5 = new Territory("territory5", 2, 3);
+
+    t1->addAdjacentTerritory(t2);
+    t1->addAdjacentTerritory(t3);
+
+    t2->addAdjacentTerritory(t1);
+    t2->addAdjacentTerritory(t4);
+
+    t3->addAdjacentTerritory(t1);
+    t3->addAdjacentTerritory(t4);
+
+    t4->addAdjacentTerritory(t2);
+    t4->addAdjacentTerritory(t3);
+    t4->addAdjacentTerritory(t5);
+
+    t5->addAdjacentTerritory(t2);
+    t5->addAdjacentTerritory(t4);
+
+    Map map;
+    map.addTerritory(t1);
+    map.addTerritory(t2);
+    map.addTerritory(t3);
+    map.addTerritory(t4);
+    map.addTerritory(t5);
+
+    bool x = map.isConnectedTerritories();
+    cout << x;
+
+
+
+    delete t1;
+    delete t2;
+    delete t3;
+    delete t4;
+    delete t5;
 }
--- Map.h
+++ Map.h
@@ -3,10 +3,6 @@
 using std::string;
 using std::vector;
 
-class Map{
-
-};
-
 class Continent{
 
 };
@@ -24,16 +20,17 @@ class Territory{
 
     public:
         Territory();
-        Territory(Territory &t);
+        Territory(const Territory &t);
+        Territory(string name, int x, int y);
         Territory(Player* owner, Continent* continent, int numOfArmies, string name, vector<Territory*>, int x, int y);
-
-        Player* getOwner();
-        Continent* getContinent();
-        int getNumOfArmies();
-        string getName();
-        vector<Territory*> getAdjacentTerritories();
-        int getX();
-        int getY();
+        Territory(Player *owner, Continent* continent, int numOfArmies, string name, int x, int y);
+        Player* getOwner() { return owner; }
+        Continent* getContinent() { return continent; }
+        int getNumOfArmies() { return numOfArmies; }
+        string getName() { return name; }
+        vector<Territory*> getAdjacentTerritories() { return adjacentTerritories; }
+        int getX() { return x; }
+        int getY() { return y; }
 
         void setOwner(Player* player);
         void setContinent(Continent* continent);
@@ -44,6 +41,29 @@ class Territory{
         void setY(int y);
 
         void addAdjacentTerritory(Territory* territory);
+};
+
+class Map{
+    private:
+        vector <Territory*> allTerritories;
+        vector <Continent*> allContinents;
+        void dfs(vector<string>* visitedTerritories, Territory* currentTerritory);
+
+    public:
+        Map();
+        Map(const Map &map);
+
+        vector<Territory*> getAllTerritories() { return allTerritories; }
+        vector<Continent*> getAllContinents() { return allContinents; }
+        void setAllTerritories(vector<Territory*> territories);
+        void setAllContinents(vector<Continent*> continents);
+
+        void addTerritory(Territory* t);
+        void addContinent(Continent* c);
+        bool isConnectedTerritories();
+        bool isConnectedContinents();
+        bool validate();
+
 
 
 
