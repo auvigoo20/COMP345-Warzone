99079232608f2f171fc0dee9ff4b779979608d37
srauph <47877347+srauph@users.noreply.github.com>
2022-10-01 20:27:10 -0400

    Added classes to GameEngine

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,1 +1,97 @@
 #include "GameEngine.h"
+
+Transition::Transition() {
+    name = nullptr;
+    to = nullptr;
+}
+
+Transition::Transition(const Transition &t) {
+    name = t.name;
+    to = t.to;
+}
+
+Transition::Transition(std::string *name, State *to) {
+    this->name = name;
+    this->to = to;
+}
+
+string *Transition::getName() {
+    return name;
+}
+
+State *Transition::getTo() {
+    return to;
+}
+
+void Transition::setName(std::string *name) {
+    this->name = name;
+}
+
+void Transition::setTo(State *to) {
+    this->to = to;
+}
+
+State::State() {
+    name = nullptr;
+}
+
+State::State(const State &s) {
+    name = s.name;
+    transitions = s.transitions;
+}
+
+State::State(std::string *name, vector<Transition *> transitions) {
+    this->name = name;
+    this->transitions = transitions;
+}
+
+string *State::getName() {
+    return name;
+}
+
+vector<Transition *> State::getTransitions() {
+    return transitions;
+}
+
+void State::setName(std::string *name) {
+    this->name = name;
+}
+
+void State::setTransitions(vector<Transition *> transitions) {
+    this->transitions = transitions;
+}
+
+GameEngine::GameEngine() {
+    currentState = nullptr;
+    latestCommand = nullptr;
+}
+
+GameEngine::GameEngine(const GameEngine &g) {
+    currentState = g.currentState;
+    latestCommand = g.latestCommand;
+}
+
+GameEngine::GameEngine(State *startingState) {
+    currentState = startingState;
+    latestCommand = nullptr;
+}
+
+State *GameEngine::getCurrentState() {
+    return currentState;
+}
+
+string *GameEngine::getLatestCommand() {
+    return latestCommand;
+}
+
+void GameEngine::setCurrentState(State *currentState) {
+    this->currentState = currentState;
+}
+
+void GameEngine::setLatestCommand(std::string *latestCommand) {
+    this->latestCommand = latestCommand;
+}
+
+void GameEngine::startGame() {
+
+}
---
+++ GameEngine.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+// Declared here (forward declaration) because Transition needs to know State exists.
+class State;
+
+class Transition {
+private:
+    string* name;
+    State* to;
+public:
+    Transition();
+    Transition(const Transition &t);
+    Transition(string* name, State* to);
+
+    string* getName();
+    State* getTo();
+    void setName(string* name);
+    void setTo(State* to);
+};
+
+class State {
+private:
+    string* name;
+    vector<Transition*> transitions;
+public:
+    State();
+    State(const State &s);
+    State(string* name, vector<Transition*> transitions);
+
+    string* getName();
+    vector<Transition*> getTransitions();
+    void setName(string* name);
+    void setTransitions(vector<Transition*> transitions);
+};
+
+class GameEngine {
+private:
+    State* currentState;
+    string* latestCommand;
+public:
+    GameEngine();
+    GameEngine(const GameEngine &g);
+    GameEngine(State* startingState);
+
+    State* getCurrentState();
+    string* getLatestCommand();
+    void setCurrentState(State* currentState);
+    void setLatestCommand(string* latestCommand);
+
+    void startGame();
+};
+
+State* start;
+State* mapLoaded;
