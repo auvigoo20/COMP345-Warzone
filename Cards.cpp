db180bfd44542957a519ad5dbc4fd23d33fa31da
MarieHaz <marie.hazari@hotmail.ca>
2022-10-02 15:48:00 -0400

    Update classes .2

--- Cards.cpp
+++ Cards.cpp
@@ -1,7 +1,7 @@
 #include "Cards.h"
 #include "Map.h"
-
 #include <iostream>
+
 using std::cout;
 using std::endl;
 
@@ -9,37 +9,72 @@ int testing(){
     return 123;
 }
 
-Bomb::Bomb(){
+Deck::Deck(const Deck& o){
+    this -> deckList = o.deckList;
+}
 
+void Deck::addCard(Card* card) {
+    deckList.push_back(card);
+}
+
+void Deck::removeCard(int index) {
+    deckList.erase(deckList.begin() + index);
+}
+
+vector<Card *> Deck::getDeckList() {
+    return deckList;
+}
+
+void Deck::draw(Hand* hand) {
+    int index = rand() % deckList.size();
+    Card* cardDraw = deckList[index];
+    removeCard(index);
+    hand->addCard(cardDraw);
+}
+
+Hand::Hand(const Hand& o){
+    this -> handList = o.handList;
+}
+
+void Hand::addCard(Card* card) {
+    handList.push_back(card);
+
+}
+void Hand::removeCard(Card* card) {
+
+}
+void Hand::playCard(Card* card){
+    card->play();
+    removeCard(card);                //remove card from hand player
+    deck->addCard(card);            // add card to deck
+}
+
+Bomb::Bomb(){
 }
 Reinforcement::Reinforcement() {
-
 }
-Blockage::Blockage() {
-
+Blockade::Blockade() {
 }
 Airlift::Airlift() {
-
 }
 Diplomacy::Diplomacy() {
-
 }
 
-
-void Bomb::play() {
+void Bomb::play(){
+//create order bomb
 
 }
 void Reinforcement::play() {
-
+//create order reinforcement
 }
-void Blockage::play() {
-
+void Blockade::play() {
+//create order blockade
 }
 void Airlift::play() {
-
+//create order airlift
 }
 void Diplomacy::play() {
-
+//create order negotiate
 }
 
 
--- Cards.h
+++ Cards.h
@@ -1,49 +1,85 @@
 #ifndef CARDS_H
 #define CARDS_H
-#include <list>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
 
 int testing();
+class Hand;
+class Card;
 
 class Deck{
 public:
-    list<Card> deckList;
+    Deck();
+    Deck(const Deck& o);
+    void addCard(Card* card);  //card being put back in the deck after play();
+    void removeCard(int index); //random card is removed from deckList at draw();
+    void draw(Hand* hand);
+    vector<Card*> getDeckList();
+    vector<Card*> deckList;
 };
 
 class Hand{
-    list<Card> playerHandList;
+public:
+    Hand();
+    Hand(const Hand& o);
+    void addCard(Card* card); //card is added in player's handList
+    void removeCard(Card* card); //card is removed from hand to be played
+    void playCard(Card* card);
+
+
+private:
+    vector<Card*> handList;
+    Deck* deck;
 };
 
 class Card{
+public:
+    virtual void play() const = 0;
+    Hand* handPlayer;
 };
 
-class Bomb : Card{
+class Bomb : public Card{
 public:
     Bomb();
+    Bomb(const Bomb& b);
     void play();
+
 };
 
-class Reinforcement : Card{
+class Reinforcement : public Card{
 public:
     Reinforcement();
+    Reinforcement(const Reinforcement& r);
     void play();
+
 };
 
-class Blockage : Card{
+class Blockade : public Card{
 public:
-    Blockage();
+    Blockade();
+    Blockade(const Blockade& bl);
     void play();
+
  };
 
-class Airlift : Card{
+class Airlift : public Card{
 public:
     Airlift();
+    Airlift(const Airlift& a);
     void play();
+
 };
 
-class Diplomacy : Card{
+class Diplomacy : public Card{
 public:
     Diplomacy();
+    Diplomacy(const Diplomacy& d);
     void play();
+
 };
 
 #endif
