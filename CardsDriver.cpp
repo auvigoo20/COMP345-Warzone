ec515d3c8003ada8cd5ccadb30d3190a388b3ec3
Auvigoo <auvigooa@gmail.com>
2022-10-07 20:12:25 -0400

    show player orders in card driver test

--- CardsDriver.cpp
+++ CardsDriver.cpp
@@ -7,7 +7,7 @@ using std::cout;
 using std::endl;
 
 int testCards(){
-    cout << "Testing Cards, HandLists and DeckLists" << endl;
+    cout << "Testing Cards, Hands and Decks" << endl;
 
     /**
      * Creating different type of Cards
@@ -32,65 +32,63 @@ int testCards(){
      * Creating a Deck
      */
 
-    Deck* deckList = new Deck();
+    Deck* deck = new Deck();
 
     /**
      * Adding all created Cards in the common Deck
      */
 
-    deckList->addCard(bomb1);
-    deckList->addCard(bomb2);
-    deckList->addCard(reinforcement1);
-    deckList->addCard(reinforcement2);
-    deckList->addCard(blockade1);
-    deckList->addCard(blockade2);
-    deckList->addCard(airlift1);
-    deckList->addCard(airlift2);
-    deckList->addCard(diplomacy1);
-    deckList->addCard(diplomacy2);
+    deck->addCard(bomb1);
+    deck->addCard(bomb2);
+    deck->addCard(reinforcement1);
+    deck->addCard(reinforcement2);
+    deck->addCard(blockade1);
+    deck->addCard(blockade2);
+    deck->addCard(airlift1);
+    deck->addCard(airlift2);
+    deck->addCard(diplomacy1);
+    deck->addCard(diplomacy2);
 
-    cout << *deckList;
 
-    /**
-     * Creating first player Hand List
-     */
-    Hand* handList1  = new Hand(deckList);
+    cout << *deck;
 
     /**
-     * Creating second player Hand List
+     * Creating first player Hand
      */
-
-    Hand* handList2 = new Hand(deckList);
+    Hand* hand1  = new Hand(deck);
 
     /**
      * Create dummy player
      */
 
     OrdersList* ordersList = new OrdersList();
-    Player* player = new Player("Bobby", handList1, ordersList);
-    handList1->setOwner(player);
+    Player* player = new Player("Bobby", hand1, ordersList);
+    hand1->setOwner(player);
 
     /**
      * Testing draw() method on first player Hand ; player draw a Card and places it in his Hands
      */
 
-    cout << "//testing draw() method on player 1 Hand List" << endl;
+    cout << "Calling draw() for player Bobby" << endl;
 
-    deckList->draw(handList1);
+    deck->draw(hand1);
 
-    cout <<*deckList << endl;
-    cout <<*handList1 << endl;
+    cout << *deck << endl;
+    cout << *hand1 << endl;
 
     /**
      * Testing play() method with first player Hand
      */
 
-    cout << "//testing player play() method on this card" << endl;
+    cout << "Player Bobby calling the play() method on this card" << endl;
 
-    handList1->playCard(1);
+    hand1->playCard(1);
 
-    cout <<*deckList << endl;
-    cout <<*handList1 << endl;
+    cout << *deck << endl;
+    cout << *hand1 << endl;
+
+    cout << "Check if the order associated with the played card is in the Player's orderlist" << endl;
+    cout << *player->getOrdersList() << endl;
 
     return 0;
 }
