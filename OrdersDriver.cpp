8eba7678f40f8ea79097788380d5d344ebced076
Ali Hanni <alihanni95@gmail.com>
2022-11-13 22:15:56 -0500

    Fixed minor bug on Orders execution driver

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -261,15 +261,6 @@ int testOrderExecution(){
     invalidBlockade->execute();
     cout << '\n' << endl;
 
-    // Testing correct behavior for Advance order
-    cout << " -- Testing that territory ownership is transferred given successful attack" << endl;
-    Order* successfulAttack = new Advance(player1, 5, ownedTerritory1, enemyTerritory1);
-    cout << "Print 'enemyTerritory1 owner before successful attack" << endl;
-    cout << enemyTerritory1->getOwner()->getName() << endl;
-    successfulAttack->execute();
-    cout << "Print 'enemyTerritory1 owner after successful attack" << endl;
-    cout << enemyTerritory1->getOwner()->getName() << endl;
-    cout << "\n" << endl;
 
     //TESTING NEGOTIATE ORDER VALIDATION
 
@@ -289,6 +280,19 @@ int testOrderExecution(){
     Order* invalidNegotiate = new Negotiate(player1, player1);
     invalidNegotiate->execute();
     cout << '\n' << endl;
+
+    // Testing correct behavior for Advance order
+    cout << " -- Testing that territory ownership is transferred given successful attack" << endl;
+    player1->setAllyPlayerList({});
+    enemyTerritory1->setOwner(player2);
+    Order* successfulAttack = new Advance(player1, 5, ownedTerritory1, enemyTerritory1);
+    cout << "Print 'enemyTerritory1 owner before successful attack" << endl;
+    cout << enemyTerritory1->getOwner()->getName() << endl;
+    successfulAttack->execute();
+    cout << "Print 'enemyTerritory1 owner after successful attack" << endl;
+    cout << enemyTerritory1->getOwner()->getName() << endl;
+    cout << "\n" << endl;
+
 
     delete player1;
     delete player2;
