bd7b4998fc31dda9c90d0288cd55eadfaa8746d8
Auvigoo <auvigooa@gmail.com>
2022-11-13 19:51:34 -0500

    missing logging observer constructors and operators

--- LoggingObserver.h
+++ LoggingObserver.h
@@ -1,21 +1,27 @@
 #pragma once
 #include <string>
 #include <list>
+#include <iostream>
 using std::string;
 using std::list;
+using std::ostream;
 
 class ILoggable{
     public:
+        virtual ~ILoggable();
         virtual string stringToLog() = 0;
 };
+ostream& operator<<(ostream &strm, const ILoggable &il);
 
 class Observer{
     public:
-        ~Observer();
+        virtual ~Observer();
         virtual void update(ILoggable*) = 0;
     protected:
         Observer();
 };
+
+ostream& operator<<(ostream &strm, const Observer &o);
 
 class Subject{
     public:
@@ -24,11 +30,19 @@ class Subject{
         virtual void notify(ILoggable* il);
         Subject();
         ~Subject();
-    private:
+        Subject(const Subject &s);
+        Subject& operator =(const Subject& s);
+
+private:
         list<Observer*>* observers;
+        friend ostream& operator<<(ostream&, const Subject&);
 };
+
+ostream& operator<<(ostream &strm, const Subject &s);
 
 class LogObserver: public Observer{
     public:
+        LogObserver();
         void update(ILoggable*);
 };
+ostream& operator<<(ostream &strm, const LogObserver &lo);
