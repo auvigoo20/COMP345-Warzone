827889c247bd14642818bf56483adf1628dd9bf5
Ali Hanni <alihanni95@gmail.com>
2022-12-02 13:55:51 -0500

    Added assignment opp, output stream opp, and copy constructors to all strategy classes

--- PlayerStrategies.cpp
+++ PlayerStrategies.cpp
@@ -16,9 +16,35 @@ HumanPlayerStrategy::HumanPlayerStrategy(Player *player)
    this->player = player;
 }
 
+/**
+ * Copy constructor
+ * @param s Strategy to copy
+ */
+HumanPlayerStrategy::HumanPlayerStrategy(const HumanPlayerStrategy &s)
+{
+    this->player = s.player;
+}
+
 // Destructor
 HumanPlayerStrategy::~HumanPlayerStrategy()
 {
+}
+
+/**
+ * Assignment Operator
+ * @param s
+ * @return
+ */
+HumanPlayerStrategy& HumanPlayerStrategy::operator=(const HumanPlayerStrategy& s) {
+    if(this != &s) {
+        this->player = s.player;
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& output, HumanPlayerStrategy& s)
+{
+    output << "Human Player Strategy" << endl;
 }
 
 
@@ -34,7 +60,6 @@ void HumanPlayerStrategy::setPlayer(Player *player)
 {
     this->player = player;
 }
-
 
 
 // Aggressive Player Strategy
@@ -53,11 +78,36 @@ AggressivePlayerStrategy::AggressivePlayerStrategy(Player *player)
     this->player = player;
 }
 
+/**
+ * Copy constructor
+ * @param s Strategy to copy
+ */
+AggressivePlayerStrategy::AggressivePlayerStrategy(const AggressivePlayerStrategy &s)
+{
+    this->player = s.player;
+}
+
 // Destructor
 AggressivePlayerStrategy::~AggressivePlayerStrategy()
 {
 }
 
+/**
+ * Assignment Operator
+ * @param s
+ * @return
+ */
+AggressivePlayerStrategy& AggressivePlayerStrategy::operator=(const AggressivePlayerStrategy& s) {
+    if(this != &s) {
+        this->player = s.player;
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& output, AggressivePlayerStrategy& s)
+{
+    output << "Aggressive Player Strategy" << endl;
+}
 
 // Getters and setters
 
@@ -87,11 +137,37 @@ BenevolentPlayerStrategy::BenevolentPlayerStrategy(Player *player)
     this->player = player;
 }
 
+
+/**
+ * Copy constructor
+ * @param s Strategy to copy
+ */
+BenevolentPlayerStrategy::BenevolentPlayerStrategy(const BenevolentPlayerStrategy &s)
+{
+    this->player = s.player;
+}
+
 // Destructor
 BenevolentPlayerStrategy::~BenevolentPlayerStrategy()
 {
 }
 
+/**
+ * Assignment Operator
+ * @param s
+ * @return
+ */
+BenevolentPlayerStrategy& BenevolentPlayerStrategy::operator=(const BenevolentPlayerStrategy& s) {
+    if(this != &s) {
+        this->player = s.player;
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& output, BenevolentPlayerStrategy& s)
+{
+    output << "Benevolent Player Strategy" << endl;
+}
 
 // Getters and setters
 
@@ -121,11 +197,37 @@ NeutralPlayerStrategy::NeutralPlayerStrategy(Player *player)
     this->player = player;
 }
 
+
+/**
+ * Copy constructor
+ * @param s Strategy to copy
+ */
+NeutralPlayerStrategy::NeutralPlayerStrategy(const NeutralPlayerStrategy &s)
+{
+    this->player = s.player;
+}
+
 // Destructor
 NeutralPlayerStrategy::~NeutralPlayerStrategy()
 {
 }
 
+/**
+ * Assignment Operator
+ * @param s
+ * @return
+ */
+NeutralPlayerStrategy& NeutralPlayerStrategy::operator=(const NeutralPlayerStrategy& s) {
+    if(this != &s) {
+        this->player = s.player;
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& output, NeutralPlayerStrategy& s)
+{
+    output << "Neutral Player Strategy" << endl;
+}
 
 // Getters and setters
 
@@ -154,9 +256,36 @@ CheaterPlayerStrategy::CheaterPlayerStrategy(Player *player)
     this->player = player;
 }
 
+
+/**
+ * Copy constructor
+ * @param s Strategy to copy
+ */
+CheaterPlayerStrategy::CheaterPlayerStrategy(const CheaterPlayerStrategy &s)
+{
+    this->player = s.player;
+}
+
 // Destructor
 CheaterPlayerStrategy::~CheaterPlayerStrategy()
 {
+}
+
+/**
+ * Assignment Operator
+ * @param s
+ * @return
+ */
+CheaterPlayerStrategy& CheaterPlayerStrategy::operator=(const CheaterPlayerStrategy& s) {
+    if(this != &s) {
+        this->player = s.player;
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& output, CheaterPlayerStrategy& s)
+{
+    output << "Cheater Player Strategy" << endl;
 }
 
 
--- PlayerStrategies.h
+++ PlayerStrategies.h
@@ -17,16 +17,19 @@ class NeutralPlayerStrategy: public PlayerStrategies {
 public:
     NeutralPlayerStrategy();
     NeutralPlayerStrategy(Player* player);
+    NeutralPlayerStrategy(const NeutralPlayerStrategy& s);
     ~NeutralPlayerStrategy();
 
     vector<Territory*> toAttack() override;
     vector<Territory*> toDefend() override;
     void issueOrder() override;
+    NeutralPlayerStrategy& operator=(const NeutralPlayerStrategy& s);
 
     void setPlayer(Player* player);
     Player* getPlayer();
 
 private:
+    friend ostream& operator<<(ostream& output, NeutralPlayerStrategy& s);
     Player* player;
 };
 
@@ -35,16 +38,19 @@ class AggressivePlayerStrategy: public PlayerStrategies {
 public:
     AggressivePlayerStrategy();
     AggressivePlayerStrategy(Player* player);
+    AggressivePlayerStrategy(const AggressivePlayerStrategy& s);
     ~AggressivePlayerStrategy();
 
     vector<Territory*> toAttack() override;
     vector<Territory*> toDefend() override;
     void issueOrder() override;
+    AggressivePlayerStrategy& operator=(const AggressivePlayerStrategy& s);
 
     void setPlayer(Player* player);
     Player* getPlayer();
 
 private:
+    friend ostream& operator<<(ostream& output, AggressivePlayerStrategy& s);
     Player* player;
 };
 
@@ -52,16 +58,19 @@ class BenevolentPlayerStrategy: public PlayerStrategies {
 public:
     BenevolentPlayerStrategy();
     BenevolentPlayerStrategy(Player* player);
+    BenevolentPlayerStrategy(const BenevolentPlayerStrategy& s);
     ~BenevolentPlayerStrategy();
 
     vector<Territory*> toAttack() override;
     vector<Territory*> toDefend() override;
     void issueOrder() override;
+    BenevolentPlayerStrategy& operator=(const BenevolentPlayerStrategy& s);
 
     void setPlayer(Player* player);
     Player* getPlayer();
 
 private:
+    friend ostream& operator<<(ostream& output, BenevolentPlayerStrategy& s);
     Player* player;
 };
 
@@ -69,16 +78,19 @@ class HumanPlayerStrategy: public PlayerStrategies {
 public:
     HumanPlayerStrategy();
     HumanPlayerStrategy(Player* player);
+    HumanPlayerStrategy(const HumanPlayerStrategy& s);
     ~HumanPlayerStrategy();
 
     vector<Territory*> toAttack() override;
     vector<Territory*> toDefend() override;
     void issueOrder() override;
+    HumanPlayerStrategy& operator=(const HumanPlayerStrategy& s);
 
     void setPlayer(Player* player);
     Player* getPlayer();
 
 private:
+    friend ostream& operator<<(ostream& output, HumanPlayerStrategy& s);
     Player* player;
 };
 
@@ -86,11 +98,13 @@ class CheaterPlayerStrategy: public PlayerStrategies {
 public:
     CheaterPlayerStrategy();
     CheaterPlayerStrategy(Player* player);
+    CheaterPlayerStrategy(const CheaterPlayerStrategy& s);
     ~CheaterPlayerStrategy();
 
     vector<Territory*> toAttack() override;
     vector<Territory*> toDefend() override;
     void issueOrder() override;
+    CheaterPlayerStrategy& operator=(const CheaterPlayerStrategy& s);
 
     void setPlayer(Player* player);
     Player* getPlayer();
